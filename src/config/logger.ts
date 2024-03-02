253c4445e7f3899720d09da3804b85221e7352ac
pulak-sarmah <pulaksarmah333@gmail.com>
2024-03-02 13:38:43 +0530

    add logger

---
+++ src/config/logger.ts
@@ -0,0 +1,35 @@
+import winston from 'winston';
+import { Config } from '.';
+
+const logger = winston.createLogger({
+    level: 'info',
+
+    defaultMeta: {
+        serviceName: 'auth-service',
+    },
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json(),
+    ),
+
+    transports: [
+        new winston.transports.File({
+            dirname: 'logs',
+            filename: 'combined.log',
+            level: 'info',
+            silent: Config.NODE_ENV === 'test',
+        }),
+        new winston.transports.File({
+            dirname: 'logs',
+            filename: 'error.log',
+            level: 'error',
+            silent: Config.NODE_ENV === 'test',
+        }),
+        new winston.transports.Console({
+            level: 'info',
+            silent: Config.NODE_ENV === 'test',
+        }),
+    ],
+});
+
+export default logger;
--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,20 @@
 import { app } from './app';
 import { Config } from './config';
+import logger from './config/logger';
 
 function startServer() {
     try {
         const PORT = Config.PORT;
         app.listen(PORT, () => {
-            // eslint-disable-next-line no-console
-            console.log(`server is running on port ${PORT}`);
+            logger.info(`server is running on port ${PORT}`);
         });
-    } catch (error) {
-        // eslint-disable-next-line no-console
-        console.error('Error starting server:', error);
-        process.exit(1);
+    } catch (error: unknown) {
+        if (error instanceof Error) {
+            logger.error(error.message);
+            setTimeout(() => {
+                process.exit(1);
+            }, 1000);
+        }
     }
 }
 
