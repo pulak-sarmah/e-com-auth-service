c6f7b08c209f1b8fce8b520fadf7fd8e6d885afc
pulak-sarmah <pulaksarmah333@gmail.com>
2024-03-02 14:46:27 +0530

    add error handler

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import logger from './config/logger';
+import { HttpError } from 'http-errors';
 
 const app = express();
 
@@ -6,4 +8,22 @@ app.get('/', (req, res) => {
     res.send('AUTH SERVICE IS RUNNING');
 });
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+    logger.error(err.message);
+
+    const statusCode = err.statusCode || 500;
+
+    res.status(statusCode).json({
+        errors: [
+            {
+                type: err.name,
+                statusCode: err.statusCode,
+                msg: err.message,
+                path: '',
+                location: '',
+            },
+        ],
+    });
+});
 export { app };
