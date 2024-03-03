de21c45cb90664ed5732d405e845ee3695e0fbb7
pulak-sarmah <pulaksarmah333@gmail.com>
2024-03-03 13:57:03 +0530

    Add registration tests

---
+++ tests/users/register.spec.ts
@@ -0,0 +1,57 @@
+import request from 'supertest';
+import { app } from '../../src/app';
+
+describe('POST /auth/register', () => {
+    describe('Given all fields', () => {
+        it('should return the 201 status code', async () => {
+            //Arrange
+            const userData = {
+                firstName: 'john',
+                lastName: 'Doe',
+                email: 'johndoe@gmail.com',
+                password: 'secret',
+            };
+            // Act
+            const response = await request(app)
+                .post('/auth/register')
+                .send(userData);
+
+            // Assert
+            expect(response.statusCode).toBe(201);
+        });
+
+        it('should return valid json data', async () => {
+            const userData = {
+                firstName: 'john',
+                lastName: 'Doe',
+                email: 'johndoe@gmail.com',
+                password: 'secret',
+            };
+            const response = await request(app)
+                .post('/auth/register')
+                .send(userData);
+
+            expect(
+                (response.headers as Record<string, string>)['content-type'],
+            ).toEqual(expect.stringContaining('json'));
+        });
+
+        it('should persist the user data in the database', async () => {
+            const userData = {
+                firstName: 'john',
+                lastName: 'Doe',
+                email: 'johndoe@gmail.com',
+                password: 'secret',
+            };
+            const response = await request(app)
+                .post('/auth/register')
+                .send(userData);
+
+            expect(
+                (response.headers as Record<string, string>)['content-type'],
+            ).toEqual(expect.stringContaining('json'));
+        });
+    });
+
+    describe('fields are missing', () => {});
+});
