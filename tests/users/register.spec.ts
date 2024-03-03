87f592cfc40b6c36648dd3d71aa66bee00881d79
pulak-sarmah <pulaksarmah333@gmail.com>
2024-03-03 16:23:59 +0530

    add tests for userRegister

--- tests/users/register.spec.ts
+++ tests/users/register.spec.ts
@@ -1,7 +1,25 @@
+import { AppDataSource } from './../../src/config/data-source';
+import { DataSource } from 'typeorm';
 import request from 'supertest';
 import { app } from '../../src/app';
+import { truncateTable } from '../utils';
+import { User } from '../../src/entity/User';
 
 describe('POST /auth/register', () => {
+    let connection: DataSource;
+
+    beforeAll(async () => {
+        connection = await AppDataSource.initialize();
+    });
+
+    beforeEach(async () => {
+        await truncateTable(connection);
+    });
+
+    afterAll(async () => {
+        await connection.destroy();
+    });
+
     describe('Given all fields', () => {
         it('should return the 201 status code', async () => {
             //Arrange
@@ -47,7 +65,14 @@ describe('POST /auth/register', () => {
                 .post('/auth/register')
                 .send(userData);
 
-            expect();
+            const userRepository = connection.getRepository(User);
+
+            const user = await userRepository.find();
+
+            expect(user).toHaveLength(1);
+            expect(user[0].firstName).toBe(userData.firstName);
+            expect(user[0].lastName).toBe(userData.lastName);
+            expect(user[0].email).toBe(userData.email);
         });
     });
 
