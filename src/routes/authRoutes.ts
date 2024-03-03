f14ef0361c03396ae187edd91612deb36f14abac
pulak-sarmah <pulaksarmah333@gmail.com>
2024-03-03 16:27:36 +0530

    add user register controller

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,9 +1,14 @@
 import express from 'express';
 import { AuthController } from '../controller/AuthController';
+import { UserService } from '../services/UserService';
+import { AppDataSource } from '../config/data-source';
+import { User } from '../entity/User';
 
 const router = express.Router();
-
-const authController = new AuthController();
+const userRepository = AppDataSource.getRepository(User);
+const userService = new UserService(userRepository);
+const authController = new AuthController(userService);
 
 router.route('/register').post((req, res) => authController.register(req, res));
+
 export default router;
