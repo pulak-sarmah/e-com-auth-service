0843f52cd41949050faa5286fa91c778b4abf8cc
pulak-sarmah <pulaksarmah333@gmail.com>
2024-03-04 23:07:30 +0530

    Add  refresh and access token to  AuthController

--- src/controller/AuthController.ts
+++ src/controller/AuthController.ts
@@ -1,9 +1,14 @@
+import fs from 'fs';
 import { NextFunction, Response } from 'express';
-import { RegisterUserRequest } from '../types';
-import { UserService } from '../services/UserService';
 import { Logger } from 'winston';
+import { UserService } from '../services/UserService';
+import { RegisterUserRequest } from '../types';
+import { JwtPayload, sign } from 'jsonwebtoken';
 
 import { validationResult } from 'express-validator';
+import path from 'path';
+import createHttpError from 'http-errors';
+import { Config } from '../config';
 
 export class AuthController {
     constructor(
@@ -38,6 +43,50 @@ export class AuthController {
             });
 
             this.logger.info('User has been registered ', { id: user.id });
+
+            let privateKey: Buffer;
+            try {
+                privateKey = fs.readFileSync(
+                    path.join(__dirname, '../../certs/private.pem'),
+                );
+            } catch (error) {
+                const err = createHttpError(
+                    500,
+                    'error while reading privateKey',
+                );
+                next(err);
+                return;
+            }
+
+            const payload: JwtPayload = {
+                sub: String(user.id),
+                role: user.role,
+            };
+
+            const accessToken = sign(payload, privateKey, {
+                algorithm: 'RS256',
+                expiresIn: '1h',
+                issuer: 'auth-service',
+            });
+            const refreshToken = sign(payload, Config.REFRESH_TOKEN_SECRET!, {
+                algorithm: 'HS256',
+                expiresIn: '1y',
+                issuer: 'auth-service',
+            });
+
+            res.cookie('accessToken', accessToken, {
+                domain: 'localhost',
+                sameSite: 'strict',
+                maxAge: 1000 * 60 * 60,
+                httpOnly: true,
+            });
+
+            res.cookie('refreshToken', refreshToken, {
+                domain: 'localhost',
+                sameSite: 'strict',
+                maxAge: 1000 * 60 * 24 * 365,
+                httpOnly: true,
+            });
 
             res.status(201).json({
                 id: user.id,
