065480b8f0f1a48dd1793db84734a21e93cf3c12
pulak-sarmah <pulaksarmah333@gmail.com>
2024-03-30 12:16:34 +0530

    change key read strategy

--- src/config/index.ts
+++ src/config/index.ts
@@ -15,6 +15,7 @@ const {
     DB_NAME,
     REFRESH_TOKEN_SECRET,
     JWKS_URI,
+    PRIVATE_KEY,
 } = process.env;
 
 export const Config = {
@@ -27,4 +28,5 @@ export const Config = {
     DB_NAME,
     REFRESH_TOKEN_SECRET,
     JWKS_URI,
+    PRIVATE_KEY,
 };
