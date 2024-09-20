dbd6c7166898eeb1a81dafd57f3a2ce31a51cf97
pulak-sarmah <pulaksarmah333@gmail.com>
2024-09-20 12:43:05 +0530

    Bug fix: fix default role

--- src/controller/AuthController.ts
+++ src/controller/AuthController.ts
@@ -27,12 +27,13 @@ export class AuthController {
             next(createHttpError(400, result.array()[0].msg as string));
             return;
         }
-        const { firstName, lastName, email, password } = req.body;
+        const { firstName, lastName, email, password, role } = req.body;
 
         this.logger.debug('New request to register a user', {
             firstName,
             lastName,
             email,
+            role,
             password: '****',
         });
 
@@ -42,7 +43,7 @@ export class AuthController {
                 lastName,
                 email,
                 password,
-                role: Roles.CUSTOMER,
+                role: role ? role : Roles.CUSTOMER,
             });
 
             this.logger.info('User has been registered ', { id: user.id });
